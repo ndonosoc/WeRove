9559140fd589388fc0bd309df4fd91ae000b1b3f
Maximo Chalbaud <maxichalbaud@gmail.com>
2020-03-02 18:22:21 -0300

    added users controller with profile paths

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     get 'signup', to: 'devise/registrations#new'
   end
   root to: 'pages#home'
+  resources :profile, :controller => "users", only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
