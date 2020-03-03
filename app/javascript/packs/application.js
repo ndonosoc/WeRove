21f955a0d01e225f71589d06bd3c6cf36c4ea090
Analida Corro <analidacorro@gmail.com>
2020-03-03 16:45:28 -0300

    algolia

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,19 @@
 import "bootstrap";
+import { initAutocomplete } from "../plugins/init_autocomplete";
+import flatpickr from 'flatpickr';
+
+initAutocomplete();
+
+
+const datepicker = document.getElementById("datepicker");
+
+flatpickr("datepicker", {
+    inline: true,
+    altInput: true,
+    enableTime: true,
+    minDate: 'today',
+    minuteIncrement: 30,
+    minTime: "8:00",
+    maxTime: "20:00",
+    disable: [weekends]
+    });
