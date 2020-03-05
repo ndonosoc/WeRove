b2a97114fc50b861d45e4d9afa455ce43e17e7d9
Xenia Boulamatsis <xenia.boulamatsis@gmail.com>
2020-03-05 19:29:05 -0300

    added availiability button and made profile prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import { initAutocomplete } from "../plugins/init_autocomplete";
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/themes/airbnb.css';
 import '../plugins/toggle_interest';
-
+import '../plugins/slidebar';
 import 'flatpickr/dist/flatpickr.min.css';
 
 initAutocomplete();
---
+++ app/javascript/plugins/slidebar.js
@@ -0,0 +1,10 @@
+const avtive =  document.querySelectorAll(".category-tag")
+
+if (tags) {
+  tags.forEach(tag => {
+    tag.addEventListener("click", (event) => {
+      tag.classList.toggle("active-tag");
+      tag.children[0].children[1].checked = "checked";
+    })
+  })
+}
