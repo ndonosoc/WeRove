ca894879400754328032ec43704d54675869c4a3
Maximo Chalbaud <maxichalbaud@gmail.com>
2020-03-06 10:08:02 -0300

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 import "bootstrap";
 import { initAutocomplete } from "../plugins/init_autocomplete";
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/themes/airbnb.css';
 
 import '../plugins/toggle_interest';
 import '../plugins/slidebar';
