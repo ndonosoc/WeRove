5ceb1d8ed40f0a493fe6b2c7aa66ac5152aa6b5b
Maximo Chalbaud <maxichalbaud@gmail.com>
2020-03-09 10:55:09 -0300

    m

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-# class ChatRoomsController < ApplicationController
-#   def show
-#     @chat_room = ChatRoom.includes(messages: :user).find(params[:id])
-#   end
-# end
-=======
 class ChatRoomsController < ApplicationController
   def show
     @chat_room = ChatRoom.includes(messages: :user).find(params[:match_id])
