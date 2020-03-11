7fa8e20f07371bc9634c8c58f40951f0fc080af4
Maximo Chalbaud <maxichalbaud@gmail.com>
2020-03-11 11:36:03 -0300

    Added default pictures if non uploaded for recommendations and users, also fixed something in the matcher

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -3,7 +3,10 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.getElementById('location');
   if (addressInput) {
-    places({ container: addressInput });
+    places({
+      appId: "plB7OCT0SN2O",
+      apiKey: "135f9a8025d3c18159ad5fa4df41bd0e",
+      container: addressInput });
   }
 };
 
