99e14ab9f852b54c7fc9915e141409b6a3f3e299
Maximo Chalbaud <maxichalbaud@gmail.com>
2020-03-11 14:20:10 -0300

    sophistified algorithm mainly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_214413) do
+ActiveRecord::Schema.define(version: 2020_03_11_151112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_214413) do
     t.datetime "updated_at", null: false
     t.boolean "tourist_seen", default: false
     t.boolean "local_seen", default: false
+    t.integer "percentage"
     t.index ["local_id"], name: "index_matches_on_local_id"
     t.index ["tourist_id"], name: "index_matches_on_tourist_id"
   end
