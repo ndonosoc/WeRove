86fa19ee480ead8139562485a542003d244bc328
Maximo Chalbaud <maxichalbaud@gmail.com>
2020-03-11 16:59:29 -0300

    keep working

--- app/controllers/registration_steps_controller.rb
+++ app/controllers/registration_steps_controller.rb
@@ -7,4 +7,11 @@ class RegistrationStepsController < ApplicationController
     @groups = Interest.where.not(parent_id: nil).group_by { |interest| interest.parent.title if interest.parent.present? }
     render_wizard
   end
+
+  private
+
+  def finish_wizard_path
+    redirect_to root_path, notice: "Thanks for signing up."
+  end
+
 end
