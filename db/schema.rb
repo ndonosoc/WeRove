e2964685da4f2e3f81b7baec2c8df187d22a31e9
Nicolas Donoso <nicodonosocastex@hotmail.com>
2020-03-11 18:45:26 -0300

    Added Facebook sign-in authentification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_214413) do
+ActiveRecord::Schema.define(version: 2020_03_11_201914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,6 +144,11 @@ ActiveRecord::Schema.define(version: 2020_03_10_214413) do
     t.float "longitude"
     t.string "country_flag"
     t.integer "age"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
