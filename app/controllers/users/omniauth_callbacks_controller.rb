4d7f5781e84f310bcb8b318d839623e494d8d0a1
Nicolas Donoso <nicodonosocastex@hotmail.com>
2020-03-12 13:57:05 -0300

    facebook signup done

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,11 +3,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
 
     if user.persisted?
-      sign_in_and_redirect user, event: :authentication
+      sign_in user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+    end
+
+    if user.city
+      return redirect_to root_path
+    else
+      return redirect_to edit_user_registration_path
     end
   end
 end
