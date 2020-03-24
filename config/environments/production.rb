a326cdb11b26c3f53d00e6aca0080600b66adde7
Maximo Chalbaud <maxichalbaud@gmail.com>
2020-03-24 19:01:21 -0300

    I need to stop paying the 7usd monthly jeje

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,10 +43,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://www.werove.world/cable'
+  config.action_cable.url = 'ws://www.werove.world/cable'
   config.action_cable.allowed_request_origins = [ "https://www.werove.world", "http://www.werove.world" ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
